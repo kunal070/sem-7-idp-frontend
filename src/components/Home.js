8236f4ab30f4bd95a085c8e7a15aaa2feb0eec57
kunal070 <kp058665@gmail.com>
2023-08-28 15:34:10 +0530

    kunal & manav merge

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,11 +16,15 @@ const Home = ({session, logout}) => {
   console.log("home: " , session)
   return (
     <div className='main-container'>
-      <p style={{color:"red"}}>phone: {session.phone}</p>
-      <p style={{color:"red"}}>firstName: {(session.firstName, " " ,session.lastName)|| session.name}</p>
-      <p style={{color:"red"}}>email: {session.email || session.emailId}</p>
-      <p style={{color:"red"}}>typeOfUser: {session.typeOfUser}</p>
-      <button onClick={logout} style={{color:'red', backgroundColor:'purple', padding:'15px 20px', margin:"20px"}} > Logout </button>
+      <div className='temp'>
+      <p style={{color:'#0f3c69',padding:'1rem'}}><b>Name :</b> {(session.firstName + " "  + session.lastName) || session.name}</p>
+      <p style={{color:'#0f3c69',padding:'1rem'}}><b>Phone :</b> {session.phone}</p>
+      <p style={{color:'#0f3c69',padding:'1rem'}}><b>E-mail :</b> {session.email || session.emailId}</p>
+      <p style={{color:'#0f3c69',padding:'1rem'}}><b>TypeOfUser :</b> {session.typeOfUser}</p>
+      <center>
+        <button onClick={logout} style={{color:'white', backgroundColor:'#0f3c69', borderRadius:'0.7rem', padding:'15px 40px'}} > <b> Logout </b> </button>
+      </center>
+      </div>
     </div>
   )
 }
